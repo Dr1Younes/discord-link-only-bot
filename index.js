156213a630f6bfc3e7ad6b9a9773327f88329648
Dr1Younes <gfagffasds@gmail.com>
2025-07-19 03:04:14 +0300

    index.js

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
+console.log("Bot is starting...");
+
 require('dotenv').config();
 const { Client, GatewayIntentBits } = require('discord.js');
+
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
-const CHANNEL_ID = 'MTM5NTg3NTIwMzQwOTY0MTU4Mg.GGyOAx.hEq02rXoYiiJVQN72a6rkjKp2bwWx1leTFvt8Q';
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
 
 client.on('messageCreate', (message) => {
-  if (message.channel.id === CHANNEL_ID && !message.author.bot) {
-    if (!message.content.match(/https?:\/\/\S+/)) {
-      message.delete();
-    }
+  if (message.author.bot) return;
+
+  const urlRegex = /(https?:\/\/[^\s]+)/;
+
+  if (!urlRegex.test(message.content)) {
+    message.delete().catch(() => {});
   }
 });
 
